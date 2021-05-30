7ae743b323c066755f8d9be6839d70395fe308e9
Molly Wilson <molly_wilson1@outlook.com>
2021-05-29 21:23:23 -0500

    stub out api calls

---
+++ routes/apiRoutes/index.js
@@ -0,0 +1,22 @@
+const router = require("express").Router();
+const { notes } = require("../../db/db");
+
+//get note
+router.get("/notes", (req, res) => {
+    res.json([]);
+});
+
+//save note
+router.post("/notes", (req, res) => {
+    // set id based on what the next index of the array will be
+    req.body.id = 42; //notes.length.toString();
+
+    res.json(req.body);
+});
+
+//delete note
+router.delete("/notes", (req, res) => {
+    res.status(202).send();
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 const htmlRoutes = require('./routes/htmlRoutes');
+const apiRoutes = require('./routes/apiRoutes');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -10,6 +11,7 @@ app.use(express.static('public'));
 
 // Routes
 // ===========================================================
+app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
 // Listener
