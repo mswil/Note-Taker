2465d5d73473837ce490b5ff850dbaf57b005e04
Molly Wilson <molly_wilson1@outlook.com>
2021-05-29 21:03:14 -0500

    set up a server to display the contents of the public directory

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+const htmlRoutes = require('./routes/htmlRoutes');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+// Routes
+// ===========================================================
+app.use('/', htmlRoutes);
+
+// Listener
+// ===========================================================
+app.listen(PORT, () => {
+    console.log(`API server now on port 3001!`);
+});
